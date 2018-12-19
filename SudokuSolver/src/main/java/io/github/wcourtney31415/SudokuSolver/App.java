bc453e892f14ca9a86998365c4477d56d6bdf1df
Wes Courtney <wcourtney31415@gmail.com>
2018-12-19 01:39:21 -0500

    Removed the unused Cell class. Removed code from main method. Looking to change the approach to better learn TDD. (Removed pre TDD code)

--- SudokuSolver/src/main/java/io/github/wcourtney31415/SudokuSolver/App.java
+++ SudokuSolver/src/main/java/io/github/wcourtney31415/SudokuSolver/App.java
@@ -1,10 +1,7 @@
 package io.github.wcourtney31415.SudokuSolver;
 
-import java.util.List;
-import java.util.ArrayList;
-
 public class App {
-	
+
 	static int[][] puzzle = {
 			{ 7, 0, 0, 5, 0, 3, 0, 0, 0 },
 			{ 0, 0, 2, 0, 7, 0, 9, 0, 0 },
@@ -16,36 +13,9 @@ public class App {
 			{ 0, 0, 6, 0, 4, 0, 1, 0, 0 },
 			{ 0, 0, 0, 6, 0, 7, 0, 0, 3 }
 	};
-	
+
 	public static void main(String[] args) {
 
-		
-
-		int columnIndex = 0;
-		while (columnIndex < 9) {
-
-			int rowIndex = 0;
-			while (rowIndex < 9) {
-
-				rowIndex++;
-			}
-
-			columnIndex++;
-		}
-		
-		
-		
-	}
-	
-	public static List<Integer> getImpossibilitiesFromRow(int row) {
-		List<Integer> retArray = new ArrayList<Integer>();
- 		for (int i = 0; i < 9; i++) {
- 			int element = puzzle[row][i];
-			if (!retArray.contains(element)) {
-				retArray.add(element);
-			}
-		}
-		return retArray;
 	}
 
 }
--- SudokuSolver/src/main/java/io/github/wcourtney31415/SudokuSolver/Cell.java
+++
@@ -1,8 +0,0 @@
-package io.github.wcourtney31415.SudokuSolver;
-
-public class Cell {
-	int[] possibilities = {};
-	Cell() {
-		
-	}
-}
