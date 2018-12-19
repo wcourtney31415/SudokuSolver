6a8eb6f5bee26c8cf9648f8832907ce74eb83be6
Wes Courtney <wcourtney31415@gmail.com>
2018-12-19 00:48:42 -0500

    Added tests to check identity, matching initializations, and validation for createing a puzzle object.

--- SudokuSolver/src/main/java/io/github/wcourtney31415/SudokuSolver/Puzzle.java
+++ SudokuSolver/src/main/java/io/github/wcourtney31415/SudokuSolver/Puzzle.java
@@ -1,18 +1,51 @@
 package io.github.wcourtney31415.SudokuSolver;
 
+import java.util.Arrays;
+
 public class Puzzle {
 
-	int[][] data;
+	private static final int MAX_ROW_LENGTH = 9;
+	private static final int MAX_COLUMN_LENGTH = 9;
+	
+	private int[][] data;
 
 	public Puzzle(int[][] testData) {
-		data = testData;
+		if (testData.length > MAX_ROW_LENGTH) {
+			throw new IllegalArgumentException("Row length too long.");
+		}
+		
+		
+		
+		
+		if (testData[0].length > MAX_COLUMN_LENGTH) {
+			throw new IllegalArgumentException("Column length too big.");
+		}
+		
+		
+		
+		
+		int rowLength = testData[0].length;
+		for(int i = 0; i < MAX_ROW_LENGTH; i++) {
+			if (rowLength != testData[i].length) {
+				throw new IllegalArgumentException("Inconsistant row lengths.");
+			}
+		}
+		
+		
+		
+		
+		data = new int[testData.length][];
+		for (int i = 0; i < testData.length; i++) {
+			int[] innerArray = testData[i];
+			data[i] = Arrays.copyOf(innerArray, innerArray.length);
+		}
 	}
 
 	@Override
 	public boolean equals(Object obj) {
 		if (obj.getClass().equals(getClass())) {
 			Puzzle other = (Puzzle) obj;
-			return data.equals(other.data);
+			return Arrays.deepEquals(data, other.data);
 		}
 		return false;
 	}
--- SudokuSolver/src/test/java/io/github/wcourtney31415/SudokuSolver/PuzzleTest.java
+++ SudokuSolver/src/test/java/io/github/wcourtney31415/SudokuSolver/PuzzleTest.java
@@ -1,31 +1,72 @@
 package io.github.wcourtney31415.SudokuSolver;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class PuzzleTest {
-	static int[][] testData = {
-			{ 7, 0, 0, 5, 0, 3, 0, 0, 0 },
-			{ 0, 0, 2, 0, 7, 0, 9, 0, 0 },
-			{ 0, 0, 0, 0, 6, 4, 7, 0, 0 },
-			{ 8, 0, 4, 0, 0, 6, 5, 0, 2 },
-			{ 5, 0, 0, 0, 0, 0, 0, 0, 4 },
-			{ 3, 0, 9, 4, 0, 0, 8, 0, 1 },
-			{ 0, 0, 5, 2, 1, 0, 0, 0, 0 },
-			{ 0, 0, 6, 0, 4, 0, 1, 0, 0 },
-			{ 0, 0, 0, 6, 0, 7, 0, 0, 3 }
-	};
+	private int[][] testData;
+
+	@Before
+	public void setup() {
+		testData = new int[][] {
+				{ 7, 0, 0, 5, 0, 3, 0, 0, 0 },
+				{ 0, 0, 2, 0, 7, 0, 9, 0, 0 },
+				{ 0, 0, 0, 0, 6, 4, 7, 0, 0 },
+				{ 8, 0, 4, 0, 0, 6, 5, 0, 2 },
+				{ 5, 0, 0, 0, 0, 0, 0, 0, 4 },
+				{ 3, 0, 9, 4, 0, 0, 8, 0, 1 },
+				{ 0, 0, 5, 2, 1, 0, 0, 0, 0 },
+				{ 0, 0, 6, 0, 4, 0, 1, 0, 0 },
+				{ 0, 0, 0, 6, 0, 7, 0, 0, 3 }
+		};
+	}
 
 	@Test
 	public void testIdentityEquals() {
 		Puzzle puzzle = new Puzzle(testData);
 		Assert.assertTrue(puzzle.equals(puzzle));
 	}
-	
+
 	@Test
 	public void testSameDataEquals() {
 		Puzzle puzzleA = new Puzzle(testData);
 		Puzzle puzzleB = new Puzzle(testData);
 		Assert.assertTrue(puzzleA.equals(puzzleB));
 	}
+
+	@Test
+	public void testMutateDataNotEquals() {
+		Puzzle puzzleA = new Puzzle(testData);
+		testData[0][0] = 8;
+		Puzzle puzzleB = new Puzzle(testData);
+		Assert.assertFalse(puzzleA.equals(puzzleB));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testThrowsExceptionIfRowsTooBig() {
+		new Puzzle(new int[19][9]);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testThrowsExceptionIfColumnsTooBig() {
+		new Puzzle(new int[9][19]);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testThrowsExceptionIfColumnsVaryInSize() {
+		testData = new int[][] {
+				{ 7, 0, 0, 5, 0, 3, 0, 0, 0 },
+				{ 0, 0, 2, 0, 0, 9, 0, 0 },
+				{ 0, 0, 0, 0, 6, 4, 7, 0, 0 },
+				{ 8, 0, 4, 0, 0, 6, 5, 0, 2 },
+				{ 5, 0, 0, 0, 0, 0, 0, 0, 4 },
+				{ 3, 0, 9, 0, 8, 0, 1 },
+				{ 0, 0, 5, 2, 1, 0, 0, 0, 0 },
+				{ 0, 0, 6, 0, 4, 0, 1, 0, 0 },
+				{ 0, 0, 0, 0, 3 }
+		};
+		new Puzzle(testData);
+	}
+
 }
