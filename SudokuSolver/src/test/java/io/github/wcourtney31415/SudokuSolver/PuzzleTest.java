1007d6981a2e6f49b1a57eb10373a33598270e38
Wes Courtney <wcourtney31415@gmail.com>
2018-12-30 20:17:54 -0500

    Refactored some of the tests to remove duplication.

--- SudokuSolver/src/test/java/io/github/wcourtney31415/SudokuSolver/PuzzleTest.java
+++ SudokuSolver/src/test/java/io/github/wcourtney31415/SudokuSolver/PuzzleTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 public class PuzzleTest {
 	private int[][] testData;
+	private Puzzle puzzle;
 
 	@Before
 	public void setup() {
@@ -20,49 +21,47 @@ public class PuzzleTest {
 				{ 0, 0, 6, 0, 4, 0, 1, 0, 0 },
 				{ 0, 0, 0, 6, 0, 7, 0, 0, 3 }
 		};
+		puzzle = new Puzzle(testData);
 	}
 
 	@Test
 	public void testIdentityEquals() {
-		Puzzle puzzle = new Puzzle(testData);
 		Assert.assertTrue(puzzle.equals(puzzle));
 	}
 
 	@Test
 	public void testSameDataEquals() {
-		Puzzle puzzleA = new Puzzle(testData);
-		Puzzle puzzleB = new Puzzle(testData);
-		Assert.assertTrue(puzzleA.equals(puzzleB));
+		Puzzle otherPuzzle = new Puzzle(testData);
+		Assert.assertTrue(puzzle.equals(otherPuzzle));
 	}
 
 	@Test
 	public void testMutateDataNotEquals() {
-		Puzzle puzzleA = new Puzzle(testData);
 		testData[0][0] = 8;
-		Puzzle puzzleB = new Puzzle(testData);
-		Assert.assertFalse(puzzleA.equals(puzzleB));
+		Puzzle otherPuzzle = new Puzzle(testData);
+		Assert.assertFalse(puzzle.equals(otherPuzzle));
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testThrowsExceptionIfRowsTooBig() {
 		new Puzzle(new int[19][9]);
 	}
-	
-	@Test (expected = IllegalArgumentException.class)
+
+	@Test(expected = IllegalArgumentException.class)
 	public void testThrowsExceptionIfRowsTooSmall() {
 		new Puzzle(new int[3][9]);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testThrowsExceptionIfColumnsTooBig() {
 		new Puzzle(new int[9][19]);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testThrowsExceptionIfColumnsTooSmall() {
 		new Puzzle(new int[9][3]);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testThrowsExceptionIfRowsVaryInSize() {
 		testData = new int[][] {
@@ -78,51 +77,44 @@ public class PuzzleTest {
 		};
 		new Puzzle(testData);
 	}
-	
+
 	@Test
 	public void testReadElement() {
-		Puzzle puzzle = new Puzzle(testData);
-		int read = puzzle.read(4,2);
+		int read = puzzle.read(4, 2);
 		Assert.assertEquals(read, 6);
 	}
-	
+
 	@Test
 	public void testSubmitAnswer() {
-		Puzzle puzzle = new Puzzle(testData);
 		int x = 3, y = 2;
-		int read = puzzle.read(x, y); 
+		int read = puzzle.read(x, y);
 		puzzle.submitAnswer(x, y, 4);
 		Assert.assertNotEquals(read, puzzle.read(x, y));
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testThrowsExceptionIfReadMethodXIsNegative() {
-		Puzzle puzzle = new Puzzle(testData);
 		puzzle.read(-1, 4);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testThrowsExceptionIfSubmitAnswerMethodXIsNegative() {
-		Puzzle puzzle = new Puzzle(testData);
 		puzzle.submitAnswer(-1, 4, 5);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testThrowsExceptionIfReadMethodYIsNegative() {
-		Puzzle puzzle = new Puzzle(testData);
 		puzzle.read(4, -1);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testThrowsExceptionIfSubmitAnswerMethodYIsNegative() {
-		Puzzle puzzle = new Puzzle(testData);
 		puzzle.submitAnswer(4, -1, 5);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testThrowsExceptionIfXIsLargerThanPuzzleWidth() {
-		Puzzle puzzle = new Puzzle(testData);
 		puzzle.read(9, 4);
 	}
-	
+
 }
