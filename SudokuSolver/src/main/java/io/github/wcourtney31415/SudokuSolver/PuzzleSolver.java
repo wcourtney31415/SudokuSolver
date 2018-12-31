dc09cb5527ac9dd192a516c8c68ffecb445d2f8a
Wes Courtney <wcourtney31415@gmail.com>
2018-12-31 05:52:01 -0500

    Added a PuzzleSolver class and a few tests regarding its initialization.

---
+++ SudokuSolver/src/main/java/io/github/wcourtney31415/SudokuSolver/PuzzleSolver.java
@@ -0,0 +1,9 @@
+package io.github.wcourtney31415.SudokuSolver;
+
+public class PuzzleSolver {
+
+	public PuzzleSolver(Puzzle puzzle) {
+		
+	}
+
+}
---
+++ SudokuSolver/src/test/java/io/github/wcourtney31415/SudokuSolver/PuzzleSolverTest.java
@@ -0,0 +1,39 @@
+package io.github.wcourtney31415.SudokuSolver;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PuzzleSolverTest {
+	private int[][] testData;
+	private Puzzle puzzle;
+	private PuzzleSolver puzzleSolver;
+
+	@Before
+	public void setup() {
+		testData = new int[][] {
+				{ 7, 0, 0, 5, 0, 3, 0, 0, 0 },
+				{ 0, 0, 2, 0, 7, 0, 9, 0, 0 },
+				{ 0, 0, 0, 0, 6, 4, 7, 0, 0 },
+				{ 8, 0, 4, 0, 0, 6, 5, 0, 2 },
+				{ 5, 0, 0, 0, 0, 0, 0, 0, 4 },
+				{ 3, 0, 9, 4, 0, 0, 8, 0, 1 },
+				{ 0, 0, 5, 2, 1, 0, 0, 0, 0 },
+				{ 0, 0, 6, 0, 4, 0, 1, 0, 0 },
+				{ 0, 0, 0, 6, 0, 7, 0, 0, 3 }
+		};
+		puzzle = new Puzzle(testData);
+		puzzleSolver = new PuzzleSolver(puzzle);
+	}
+
+	@Test
+	public void testIdentityEquals() {
+		Assert.assertTrue(puzzleSolver.equals(puzzleSolver));
+	}
+	
+	@Test
+	public void testNotEqualToNull() {
+		Assert.assertFalse(puzzleSolver.equals(null));
+	}
+	
+}
